6041a0792336210df7c9c9c1f349e36f67cce577
BoiluTheGod <arthur.poullain@gmail.com>
2021-08-18 17:05:27 +0200

    peut désormais contenir des films à l'infini + update du design

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
+//API_URL = "http://localhost:8000/api/v1/titles"
 
-//api_url = "http://localhost:8000/api/v1/titles"
+async function getMOVIES(target, arg, range_from, range_to) {
+  let page_num = 1;
+  let API_URL = `http://localhost:8000/api/v1/titles/?${arg}&page=${page_num}`;
+  console.log(API_URL);
+  console.log(page_num);
+  let response = await fetch(API_URL);
+  let data = await response.json();
+  const body = document.body;
+  const targetted_div = document.getElementById(target);
+  let compteur = 0;
+  // Lets loop from our start to our end idx
+  for (let movie_count = range_from; movie_count < range_to; movie_count++) {
+    const newdiv = document.createElement("div");
+    newdiv.classList = "movie-poster";
+    const img = document.createElement("img");
+    // Début du validity check
+    // Je vérifie si l'élément existe sur ma page
+    if (typeof data.results[movie_count] == "undefined") {
+      console.log("UNDEFINED ", movie_count);
+      page_num++;
+      API_URL = `http://localhost:8000/api/v1/titles/?${arg}&page=${page_num}`;
+      response = await fetch(API_URL);
+      data = await response.json();
+      if (movie_count % 5 == 0) {
+        movie_count = 0;
+      }
+      // Sortie de notre validity check
+    }
 
-async function getMOVIES(target,arg,range_from,range_to) {
-    const api_url ="http://localhost:8000/api/v1/titles/?"+ arg
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const body = document.body;
-    const targetted_div = document.getElementById(target);
-    
-    for (let movie_count = range_from; movie_count < range_to; movie_count++) {
-            console.log(data.results[movie_count]);
-            const newdiv = document.createElement('div');
-            newdiv.classList = "movie-poster";
-            const img = document.createElement("img");
-            img.src = data.results[movie_count].image_url;
-            img.alt = data.results[movie_count].title;
-            // img.classList = "movie-poster";
-            newdiv.appendChild(img);
-            targetted_div.appendChild(newdiv);
-            const overlay = document.createElement('div')
-            overlay.classList = "overlay"
-            newdiv.appendChild(overlay)
-        }
-    // const poster = document.getElementsByClassName(movie-poster);
-    // poster.style.display = "flex";
-    // poster.style.flexDirection = "column";
+    img.src = data.results[movie_count].image_url;
+    img.alt = data.results[movie_count].title;
+    newdiv.appendChild(img);
+    targetted_div.appendChild(newdiv);
+    const overlay = document.createElement("div");
+    overlay.classList = "overlay";
+    newdiv.appendChild(overlay);
+    //  Check if our newdiv's index is greater than 4 so we decide to auto hide it or not
+    if (compteur >= 4) {
+      newdiv.style.display = "none";
+    }
+    compteur++;
+    if (compteur == range_from + range_to) {
+      return;
+    }
+  }
 }
